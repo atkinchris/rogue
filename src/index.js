26073cb8c397206b8994061949e7ed969631cdff
Chris Atkin <atkinchris@gmail.com>
2017-10-22 18:34:35 +0100

    Add basic entities

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const entities = {
-  '0100': '#'
+  '10.10': '•',
+  '11.11': '•',
+  '10.11': '#'
 }
 
 const Board = () => (
   <div>
-    <h1>{ JSON.stringify(entities) }</h1>
+    {
+      Object.keys(entities).map(key => {
+        const [x, y] = key.split('.')
+        const style = {
+          textAlign: 'center',
+          width: '1em',
+          height: '1em',
+          position: 'absolute',
+          transform: `translate(${x}em, ${y}em)`
+        }
+
+        return (
+          <div key={key} style={style}>{ entities[key] }</div>
+        )
+      })
+    }
   </div>
 )
 
