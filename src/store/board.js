16e8a24084b6dd371d75d351d22c580ead152feb
Chris Atkin <atkinchris@gmail.com>
2017-10-26 20:31:36 +0100

    Add rough movement

--- src/store/board.js
+++ src/store/board.js
@@ -21,7 +21,43 @@ const entities = board.reduce((out, row, y) => ({
 
 const boardReducer = (state = entities, action) => {
   if (action && action.type === 'input') {
-    return state
+    const playerPosition = Object.keys(state).find(key => state[key] === '@')
+    const [xStr, yStr] = playerPosition.split(',')
+    let x = parseInt(xStr, 10)
+    let y = parseInt(yStr, 10)
+
+    switch (action.payload) {
+      case 'KeyW':
+      case 'ArrowUp':
+        y -= 1
+        break
+      case 'KeyS':
+      case 'ArrowDown':
+        y += 1
+        break
+      case 'KeyA':
+      case 'ArrowLeft':
+        x -= 1
+        break
+      case 'KeyD':
+      case 'ArrowRight':
+        x += 1
+        break
+      default:
+        break
+    }
+
+    const newPosition = `${x},${y}`
+
+    if (state[newPosition] !== ' ') {
+      return state
+    }
+
+    return {
+      ...state,
+      [playerPosition]: ' ',
+      [newPosition]: '@',
+    }
   }
 
   return state
