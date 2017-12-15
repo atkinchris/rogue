451d82f287e8afd5da3c3bf0965c711e504b88e6
Chris Atkin <atkinchris@gmail.com>
2017-12-15 12:49:42 +0000

    Index entity components

--- src/utils/arraysIntersect.js
+++
@@ -1,7 +0,0 @@
-const arraysIntersect = (_arrays) => {
-  const arrays = _arrays.sort((a, b) => a.length - b.length)
-
-  return arrays.shift().filter(v => arrays.every(a => a.indexOf(v) !== -1))
-}
-
-export default arraysIntersect
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,10 @@
 import { v4 as uuid } from 'uuid'
 
 import { EnergyQueue } from './Queue'
-import arraysIntersect from './arraysIntersect'
 
 class Store {
   constructor({ debug, middleware = {} } = {}) {
-    this.entities = new Set()
+    this.entities = new Map()
     this.components = {}
     this.caches = {}
     this.turnQueue = new EnergyQueue()
@@ -18,7 +17,7 @@ class Store {
 
   createEntity() {
     const id = uuid()
-    this.entities.add(id)
+    this.entities.set(id, {})
 
     return id
   }
@@ -39,33 +38,37 @@ class Store {
 
   addComponent(entity, componentName, state = true) {
     const component = this.components[componentName] || new Map()
+    const existingComponents = this.entities.get(entity)
 
     const previous = component.get(entity)
     const next = state
 
     this.middleware.onAdd.forEach(m => m(this, componentName, entity, { previous, next }))
     component.set(entity, next)
+    this.entities.set(entity, { ...existingComponents, [componentName]: true })
 
     this.components[componentName] = component
   }
 
   removeComponent(entity, componentName) {
     const component = this.components[componentName] || new Map()
+    const existingComponents = this.entities.get(entity)
 
     this.middleware.onRemove.forEach(m => m(this, componentName, entity))
     component.delete(entity)
+    this.entities.set(entity, { ...existingComponents, [componentName]: null })
 
     this.components[componentName] = component
   }
 
   getEntitiesWith(componentNames) {
-    const entities = componentNames.map((name) => {
-      const component = this.components[name]
+    return [...this.entities].reduce((out, [entity, components]) => {
+      if (componentNames.every(name => components[name])) {
+        out.push(entity)
+      }
 
-      return component ? [...component.keys()] : []
-    })
-
-    return arraysIntersect(entities)
+      return out
+    }, [])
   }
 
   getComponent(entity, component) {
