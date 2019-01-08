ebe99a4595678214a7c1a29011e05e2eb8057e10
Chris Atkin <atkinchris@gmail.com>
2019-01-08 08:30:56 +0000

    Add newer project tooling

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const merge = require('webpack-merge')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const paths = {
   SRC: path.resolve(__dirname, 'src'),
@@ -13,7 +14,7 @@ const paths = {
 
 const common = {
   entry: {
-    main: paths.SRC,
+    main: ['@babel/polyfill', paths.SRC],
   },
   output: {
     path: paths.DEST,
@@ -22,46 +23,58 @@ const common = {
     chunkFilename: '[name].[chunkhash:8].js',
   },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: 'babel-loader',
-    }, {
-      test: /\.css$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        'css-loader',
-      ],
-    }],
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:8].css',
-    }),
+    new MiniCssExtractPlugin({ fileName: '[name].[contenthash:8].css' }),
     new HtmlWebpackPlugin({ template: path.join(paths.SRC, 'index.html') }),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 }
 
 const development = {
+  mode: 'development',
   devServer: {
     historyApiFallback: true,
     inline: true,
     stats: 'errors-only',
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({ debug: true }),
-  ],
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true }), new webpack.NamedModulesPlugin()],
   devtool: 'cheap-module-source-map',
 }
 
 const production = {
+  mode: 'production',
   plugins: [
     new CleanWebpackPlugin([paths.DEST]),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
   devtool: 'source-map',
 }
 
