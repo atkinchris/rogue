c8c2891acac359fe38dbd9fda1d8f52fbe0ca55a
Chris Atkin <atkinchris@gmail.com>
2019-01-13 21:29:36 +0000

    Convert inputHandler to TS

--- src/inputHandler.ts
+++ src/inputHandler.ts
@@ -9,28 +9,26 @@ const INPUT_MAP = {
 }
 
 class InputHandler {
+  keys: object
+  newEvents: boolean
+
   constructor() {
     this.keys = {}
     this.newEvents = false
 
-    const onDown = window.addEventListener(EVENT_DOWN, event => {
+    window.addEventListener(EVENT_DOWN, event => {
       if (event && !event.repeat && INPUT_MAP[event.code]) {
         this.keys[INPUT_MAP[event.code]] = true
         this.newEvents = true
       }
     })
 
-    const onUp = window.addEventListener(EVENT_UP, event => {
+    window.addEventListener(EVENT_UP, event => {
       if (event && !event.repeat && INPUT_MAP[event.code]) {
         this.keys[INPUT_MAP[event.code]] = false
         this.newEvents = true
       }
     })
-
-    this.destroy = () => {
-      window.removeEventListener(EVENT_DOWN, onDown)
-      window.removeEventListener(EVENT_UP, onUp)
-    }
   }
 
   getKeys() {
